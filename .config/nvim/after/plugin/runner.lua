743f589754ee888e47dfc0719c4a5ad51b729f90
hackxinho <hacknetsphere@gmail.com>
2023-01-21 17:44:59 -0300

    adding files

---
+++ .config/nvim/after/plugin/runner.lua
@@ -0,0 +1,12 @@
+vim.keymap.set('n', '<F10>', ':RunFile<CR>', { noremap = true, silent = false })
+
+require('code_runner').setup({
+  -- put here the commands by filetype
+  filetype = {
+		python = "python3 -u",
+        cpp = "cd $dir && g++ $fileName -o $fileNameWithoutExt && ./$fileNameWithoutExt",
+        javascript = "node",
+		typescript = "deno run",
+		rust = "cd $dir && rustc $fileName && $dir/$fileNameWithoutExt"
+	},
+})
